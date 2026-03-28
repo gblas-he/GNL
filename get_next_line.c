c06401d64f2c67586e5564bf5ae05673ab6714b2
Giovanni Blas Heredia <gblas-he@c2r11s6.42madrid.com>
2026-03-28 21:32:54 +0100

    gnlv2

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,14 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 15:53:16 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/26 19:19:07 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/28 21:32:07 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+//#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char	*get_next_line(int fd)
 {
@@ -19,6 +22,12 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int fd = open("ex.txt", O_RDONLY);
-
+	char *str;
+	while ((str = get_next_line(fd)) != NULL)
+	{
+		write(1, str, strlen(str));
+		free(str);
+	}
+	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,17 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 16:18:10 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/26 19:18:06 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/28 20:11:49 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+
+
 #include <fcntl.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd);
 
