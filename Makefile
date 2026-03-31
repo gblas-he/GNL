b639fb8a13a3d2caa4b433f4bc8f320d19bf5916
Giovanni Blas Heredia <gblas-he@c2r12s6.42madrid.com>
2026-03-31 21:13:11 +0200

    gnlv4

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/03/18 14:39:24 by jdiaz-ec          #+#    #+#              #
-#    Updated: 2026/03/31 19:18:38 by gblas-he         ###   ########.fr        #
+#    Updated: 2026/03/31 21:09:50 by gblas-he         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = get_next_line
 
-SRC = get_next_line_utils.c get_next_line.c
+SRC = get_next_line.c get_next_line_utils.c
 
 OBJS := $(SRC:%.c=%.o)
 INCLUDE = get_next_line.h
--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 15:53:16 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/31 19:18:34 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/31 21:09:15 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 #include <string.h>
 
 char	*get_next_line(int fd)
@@ -18,29 +19,33 @@ char	*get_next_line(int fd)
 	char	*buffer;
 	int		bytes_read;
 
-	buffer = ft_calloc(3 + 1, sizeof(char *));
+	buffer = ft_calloc(3 + 1, sizeof(char));
 	if (!buffer)
 		return (NULL);
 	bytes_read = read(fd, buffer, 3);
 	if (bytes_read <= 0)
+	{
+		free(buffer);
 		return (NULL);
+	}
 	return (buffer);
 }
 
 int	main(void)
 {
-	int fd = open("ex.txt", O_RDONLY);
-	char *str;
-	char *first = NULL;
+	__attribute__((unused)) int fd = open("ex.txt", O_RDONLY);
+	__attribute__((unused)) char *str;
+	__attribute__((unused)) char *first = NULL;
+	__attribute__((unused)) int count = 0;
 	while ((str = get_next_line(fd)) != NULL)
 	{
 		if (first == NULL)
 			first = strdup(str);
-		write(1, str, strlen(str));
+		count++;
+		printf("[%d] %s\n", count, str);
 		free(str);
 	}
-	write(1, "\n1º buffer: ", 14);
-	write(1, first, strlen(first));
+	printf("\n1º buffer: %s", first);
 	free(first);
 	close(fd);
 	return (0);
